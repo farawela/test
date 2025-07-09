9880877c2ad78a4d99353982409fcc004f46efcd
farawela <59802543+farawela@users.noreply.github.com>
2025-07-09 21:41:59 +0400

    Add files via upload

---
+++ test.ps1
@@ -0,0 +1,22 @@
+﻿$filePath = "D:\com.txt" # Replace with the actual file path
+
+foreach ($line in Get-Content $filePath) {
+    # Process each line here
+    $ip = $line
+    $port = 443
+
+try {
+    $socket = New-Object System.Net.Sockets.TcpClient($ip, $port)
+    if ($socket.Connected) {
+        "$ip port $port open"
+    } else {
+        "$ip port $port not open"
+    }
+    $socket.Close()
+} catch {
+    
+}
+     
+    # Add your specific logic for processing each line
+}
+
